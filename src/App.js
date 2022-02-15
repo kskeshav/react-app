e296a35544a1fab42dc2ee6929d58cc5b7c100ff
keshav <keshavsinghal594@gmail.com>
2022-02-15 22:25:09 +0530

    Init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Patient_Details from './components/Patient_Details';
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Patient_Details/>
+    </>
+  );
+}
+
+export default App;
